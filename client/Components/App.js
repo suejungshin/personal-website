27178daf72cdb758842948f4bff75bf24be068eb
Suejung Shin <suejungshin@Suejungs-MBP.attlocal.net>
2020-02-21 16:07:48 -0800

    Initial commit

---
+++ client/Components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+class App extends React.Component{
+  constructor() {
+
+  }
+
+
+
+
+  render() {
+    return <div>
+      Hello
+      <Button></Button>
+      </div>
+  }
+}
+
+export default App;
